65292fcad1e3c3444969f11bdf3aba01d4daab42
CapeSathish <77102126+CapeSathish@users.noreply.github.com>
2022-09-22 18:37:01 +0530

    Initial Commit

---
+++ src/main/java/com/capeelectric/CapeAwsMicroServicesApplication.java
@@ -0,0 +1,13 @@
+package com.capeelectric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapeAwsMicroServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapeAwsMicroServicesApplication.class, args);
+	}
+
+}
